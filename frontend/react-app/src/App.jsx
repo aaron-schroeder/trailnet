37577035ec14c824ae0086c5d4b76d683ff9b811
aaron-schroeder <aaschro@gmail.com>
2024-11-17 07:14:36 -0700

    Get react-leaflet map displaying correctly

--- frontend/react-app/src/App.jsx
+++ frontend/react-app/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import AddTrailJunction from './components/AddTrailJunction';
 import Map from './components/Map';
+import './App.css'
 
 function App() {
   return (
     <div>
       <h1>Trail Network</h1>
-      <AddTrailJunction />
+      {/* <AddTrailJunction /> */}
       <Map />
     </div>
   );
--- frontend/react-app/src/components/Map.jsx
+++ frontend/react-app/src/components/Map.jsx
@@ -3,6 +3,7 @@
 
 import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Polyline } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import { getTrailSegments } from '../services/api';
 
 const Map = () => {
@@ -10,22 +11,24 @@ const Map = () => {
 
     useEffect(() => {
         const fetchSegments = async () => {
-            const data = await getTrailSegments();
+            // const data = await getTrailSegments();
             // HACK to mock this function (comment out the code above):
-            // const data = [
-            //     {
-            //         "id": 1,
-            //         "name": "Sample Trail Segment",
-            //         "geometry": "SRID=4326;LINESTRING (-110.852 32.233, -110.85 32.234, -110.848 32.236)"
-            //     }
-            // ];
+            const data = [
+                {
+                    "id": 1,
+                    "name": "Sample Trail Segment",
+                    "coordinates": [[32.233, -110.852], [32.234, -110.85], [32.236, -110.848]]
+                    // "geometry": "SRID=4326;LINESTRING (-110.852 32.233, -110.85 32.234, -110.848 32.236)"
+                }
+            ];
             setSegments(data);
         };
         fetchSegments();
     }, []);
 
     return (
-        <MapContainer center={[0, 0]} zoom={2} style={{ height: '100vh', width: '100%' }}>
+        <div id='container'>
+        <MapContainer center={[32.233, -110.852]} zoom={16} >
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -33,11 +36,13 @@ const Map = () => {
             {segments.map(segment => (
                 <Polyline
                     key={segment.id}
-                    positions={segment.geometry.coordinates.map(coord => [coord[1], coord[0]])} // Leaflet expects [lat, lng]
+                    // positions={segment.geometry.coordinates.map(coord => [coord[1], coord[0]])} // Leaflet expects [lat, lng]
+                    positions={segment.coordinates}
                     color="blue"
                 />
             ))}
         </MapContainer>
+        </div>
     );
 };
 
