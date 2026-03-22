97c4381ae4d3ca04a5fbea00a46480d403b44fa2
Aaron <aaron@trailzealot.com>
2026-03-22 12:53:29 -0700

    Register models for use in django admin (postgis_app.models)

--- django/postgis_app/admin.py
+++ django/postgis_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.gis import admin as gis_admin
+from .models import TrailSegment, Trail, Route
 
 # Register your models here.
+@admin.register(Route)
+class RouteAdmin(gis_admin.GISModelAdmin):
+    list_display = ['name']
+
+@admin.register(TrailSegment)
+class TrailSegmentAdmin(gis_admin.GISModelAdmin):
+    list_display = ['name']
+
+@admin.register(Trail)
+class TrailAdmin(gis_admin.GISModelAdmin):
+    list_display = ['name']
