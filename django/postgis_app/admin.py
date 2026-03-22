0209ff145db7a31fc0f0edeb787814b2713feb8c
Aaron <aaron@trailzealot.com>
2026-03-22 14:01:08 -0700

    Add RouteRun to django admin

--- django/postgis_app/admin.py
+++ django/postgis_app/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.contrib.gis import admin as gis_admin
-from .models import TrailSegment, Trail, Route
+from .models import TrailSegment, Trail, Route, RouteRun
 
 # Register your models here.
 @admin.register(Route)
 class RouteAdmin(gis_admin.GISModelAdmin):
     list_display = ['name']
+
+@admin.register(RouteRun)
+class RouteRunAdmin(admin.ModelAdmin):
+    list_display = ['route', 'started_at', 'direction']
+    list_filter = ['direction', 'route']
+    date_hierarchy = 'started_at'
 
 @admin.register(TrailSegment)
 class TrailSegmentAdmin(gis_admin.GISModelAdmin):
